edfffc57463ae89a8ce117ad5cf93d3e0884fafe
Julien <julienmorue.dev@gmail.com>
2026-07-25 10:57:37 +0200

    v0.15.2 - Commands history and editing with `linenoise` library
    Changes :
    - Added `linenoise` library to `third_party` folder.
    - Reworked `run_loop()` function to fully use the `linenoise` library.
    - Added a history of the last typed commands, accessible with the up and down arrows keys.
    - Added a line editing mode, allowing users to move through typed commands with the left and right arrow keys without erasing characters.
    - Added a simple command auto-completion mode with tab key (currently it only works with the first word of a command).
    
    Dev Note :
    Full auto-completion on sub commands and a rework of the CommandMap structure will be added in next update.
    
    Stay tuned !

--- src/core/functions-run-loop.c
+++ src/core/functions-run-loop.c
@@ -1,8 +1,37 @@
 //functions-run-loop.c
 #include <unistd.h>
+#include "../../third_party/linenoise/linenoise.h"
 #include "../../include/prototypes.h"
 
-static void scan_user_entry(char* command_user, Model *model)
+static const char*	main_commands[] = {
+	"circuit",
+	"component",
+	"link",
+	"list",
+	"help",
+	"hello",
+	"quit",
+	"reset",
+	NULL
+};
+
+// Commands auto-completion (with tab) for linenoise 
+static void			linenoise_completion(const char *buf, linenoiseCompletions *lc)
+{
+	int i = 0;
+
+	while (main_commands[i] != NULL)
+	{
+		// If what the user wrote is the beggining of a command, it completes it
+		if (strncmp(buf, main_commands[i], strlen(buf)) == 0)
+		{
+			linenoiseAddCompletion(lc, main_commands[i]);
+		}
+		i++;
+	}
+}
+
+static void 		scan_user_entry(char* command_user, Model *model)
 {
 	char* args[MAX_COMMAND_ARGS] = {NULL};
 	int arg_count = 0;
@@ -11,11 +40,13 @@ static void scan_user_entry(char* command_user, Model *model)
 	while (*letter != '\0' && arg_count < MAX_COMMAND_ARGS)
 	{
 		// Skip spaces between args
-		while (*letter == ' ' || *letter == '\t' || *letter == '\n' || *letter == '\r') {
+		while (*letter == ' ' || *letter == '\t' || *letter == '\n' || *letter == '\r')
+		{
 			letter++;
 		}
 
-		if (*letter == '\0') {
+		if (*letter == '\0')
+		{
 			break;
 		}
 
@@ -25,11 +56,13 @@ static void scan_user_entry(char* command_user, Model *model)
 			letter++;
 			args[arg_count] = letter;
 
-			while (*letter != '"' && *letter != '\0' && *letter != '\n') {
+			while (*letter != '"' && *letter != '\0' && *letter != '\n') 
+			{
 				letter++;
 			}
 
-			if (*letter == '"' || *letter == '\n') {
+			if (*letter == '"' || *letter == '\n') 
+			{
 				*letter = '\0';
 				letter++;
 			}
@@ -39,11 +72,13 @@ static void scan_user_entry(char* command_user, Model *model)
 		{
 			args[arg_count] = letter;
 
-			while (*letter != ' ' && *letter != '\t' && *letter != '\n' && *letter != '\r' && *letter != '\0') {
+			while (*letter != ' ' && *letter != '\t' && *letter != '\n' && *letter != '\r' && *letter != '\0')
+			{
 				letter++;
 			}
 
-			if (*letter != '\0') {
+			if (*letter != '\0')
+			{
 				*letter = '\0';
 				letter++;
 			}
@@ -51,36 +86,51 @@ static void scan_user_entry(char* command_user, Model *model)
 		arg_count++;
 	}
 
-	if (args[0] == NULL) {
+	if (args[0] == NULL)
+	{
 		return;
 	}
 
 	exec_command(args, model, arg_count);
 }
 
-void	run_loop(Model *model)
+void			run_loop(Model *model)
 {
-	char	user_entry[100];
-	
+	char*	user_entry;
+	char	prompt[256];
+
+	linenoiseSetCompletionCallback(linenoise_completion);
+	linenoiseHistorySetMaxLen(100);
+
 	model->run_loop = true;
 
 	while(model->run_loop)
 	{
-		usleep(10000);
-		if ((model->active_circuit != NULL) && (strlen(model->active_circuit->label) > 0))		
+		printf("\n");
+
+		if ((model->active_circuit != NULL) && (strlen(model->active_circuit->label) > 0))
 		{
-			printf("\n" TERMINAL_CYAN "[" APP_NAME " " APP_VERSION "] "TERMINAL_GREEN "\"%s\"" TERMINAL_CYAN" > "TERMINAL_DEFAULT, model->active_circuit->label);
+			snprintf(prompt, sizeof(prompt), TERMINAL_CYAN "[" APP_NAME " " APP_VERSION "] "TERMINAL_GREEN "\"%s\"" TERMINAL_CYAN" > "TERMINAL_DEFAULT, model->active_circuit->label);
 		}
-		else {
-			printf("\n" TERMINAL_CYAN "[" APP_NAME " " APP_VERSION "] > "TERMINAL_DEFAULT);
+		else
+		{
+			snprintf(prompt, sizeof(prompt), TERMINAL_CYAN "[" APP_NAME " " APP_VERSION "] > "TERMINAL_DEFAULT);
 		}
 
-		if (fgets(user_entry, sizeof(user_entry), stdin) != NULL)
+		user_entry = linenoise(prompt);
+
+		if (user_entry == NULL) 
 		{
+			break;
+		}
+
+		if (user_entry[0] != '\0')
+		{
+			linenoiseHistoryAdd(user_entry);
 			scan_user_entry(user_entry, model);
 		}
+		free(user_entry);
 	}
 
 	return;
 }
-
